1d4b407c448cf663241afa27ce06ffcd43addb7f
gauravaroraleo <gauravaroraleo@gmail.com>
2022-03-23 19:44:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+    c1 = createCanvas(500, 500)
+    c1.position(760, 100)
+    v1 = createCapture(VIDEO)
+
+}
+
+function draw() {
+    background("gray")
+    text("SIDDHANT", 100, 100)
+    textSize(50)
+
+
+
+}
